091dc77052dc34045a0b42d3cee8eecf96a2d124
Surasith <surasith@Surasiths-MBP.lan>
2020-02-27 00:43:34 +0700

    Update Readme

--- app.js
+++ app.js
@@ -48,9 +48,3 @@ soap.createClient(url, options, function(err, client) {
     }
   });
 });
-
-// search VAT data in this link https://www.rd.go.th/publish/313.0.html
-// search VRT data in this link  https://www.rd.go.th/publish/28015.0.html
-// all Web Services of The Revenue Department http://www.rd.go.th/publish/42546.0.html
-// VAT Services http://www.rd.go.th/publish/42535.0.html
-// Example code https://medium.com/@mchayapol/ดึงข้อมูลผู้เสียภาษี-จากเลขประจำตัวผู้เสียภาษี-จาก-webservice-ของกรมสรรพากร-ด้วย-nodejs-7f91f164e7da
